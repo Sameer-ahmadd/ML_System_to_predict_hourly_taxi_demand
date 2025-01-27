872a38cf692b7d509fb81724eb408f757620950f
Sameer-ahmadd <samimughal7786@gmail.com>
2025-01-27 18:22:13 +0500

    src files added

---
+++ src/paths.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+# import sys
+# sys.path.append("..\src")
+
+PARENT_DIR = Path(__file__).parent.resolve().parent
+DATA_DIR = PARENT_DIR / 'data'
+RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
+TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
+DATA_CACHE_DIR = PARENT_DIR / 'data' / 'cache'
+
+MODELS_DIR = PARENT_DIR / 'models'
+
+if not Path(DATA_DIR).exists():
+    os.mkdir(DATA_DIR)
+
+if not Path(RAW_DATA_DIR).exists():
+    os.mkdir(RAW_DATA_DIR)
+
+if not Path(TRANSFORMED_DATA_DIR).exists():
+    os.mkdir(TRANSFORMED_DATA_DIR)
+
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
+
+if not Path(DATA_CACHE_DIR).exists():
+    os.mkdir(DATA_CACHE_DIR)
